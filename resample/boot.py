5f97491480875cbcdfdb96af0fa79872885f136c
cenonn <rm.cenon@gmail.com>
2018-01-10 20:23:45 -0800

    fixed multivariate functionality

--- resample/boot.py
+++ resample/boot.py
@@ -1,25 +1,38 @@
 import numpy as np
 import pandas as pd
 
-from .metric import Metric
+from .metric import UnivariateMetric
+from .metric import MultivariateMetric
 
 
 def boot(data, statistic, r=10000):
+    r=int(r)
     results = []
     sample_size = len(data)
     if isinstance(data, np.ndarray):
-        for _ in range(r):
-            boot_sample = [data[i] for i in np.random.randint(sample_size, size=sample_size)]
-            results.append(statistic(boot_sample))
-        return Metric(results, statistic)
-    elif isinstance(data, pd.DataFrame): 
-        for _ in range(r):
-            boot_sample = [data[0][i] for i in np.random.randint(sample_size, size=sample_size)]
-            results.append(statistic(boot_sample))
-        return Metric(results, statistic)
+        if data.ndim == 1:
+            for _ in range(r):
+                boot_sample = np.random.choice(data, sample_size, replace=True)
+                results.append(statistic(boot_sample))
+            return UnivariateMetric(results, statistic, statistic(data))
+        else:
+            raise Exception("numpy array must contain only 1 variable")
     elif isinstance(data, pd.Series): 
         for _ in range(r):
-            boot_sample = [data[i] for i in np.random.randint(sample_size, size=sample_size)]
+            boot_sample = data.sample(n=sample_size, replace=True)
             results.append(statistic(boot_sample))
-        return Metric(results, statistic)
+        return UnivariateMetric(results, statistic, statistic(data))
+    elif isinstance(data, pd.DataFrame): 
+        if len(data.columns) == 1:
+            for _ in range(r):
+                boot_sample = data.sample(n=sample_size, replace=True).as_matrix()
+                results.append(statistic(boot_sample))
+            return UnivariateMetric(results, statistic, statistic(data))
+        else: 
+            for _ in range(r):
+                boot_sample = data.sample(n=sample_size, replace=True)
+                results.append(boot_sample.apply(statistic))
+            return MultivariateMetric(results, statistic, statistic(data))
+    else:
+        raise Exception("data type not supported")
 
--- resample/metric.py
+++ resample/metric.py
@@ -1,18 +1,62 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 from scipy import stats
 
 
-class Metric:
-    def __init__(self, results, statistic):
-        self.results = results
+class UnivariateMetric:
+    def __init__(self, results, statistic, observed):
+        self.results = np.array(results)
         self.statistic = statistic
+        self.observed = observed
 
     def se(self):
-        return np.array(self.results).std(ddof=1)
+        return self.results.std(ddof=1)
+
+    def point_estimate(self, center):
+        return center(self.results)
     
-    def plot(self):
-        plt.hist(self.results, bins=30)
-        
+    def plot(self, bins=30):
+        plt.hist(self.results, bins=bins)
+
+    def ci(self, confidence=0.95, kind='efron'):
+        quantile = 100*(1 - confidence)/2
+        res = self.results
+        L, U = np.percentile(res, quantile), np.percentile(res, 100 - quantile)
+        if kind == 'efron':
+            return L, U
+        elif kind == 'loc_percentile':
+            return 2*self.observed - U, 2*self.observed - L
+        elif kind == 'scale_percentile':
+            return (self.observed ** 2 / U, self.observed ** 2 / L)
+        else:
+            raise Exception("unsupported ci type")
+            
+class MultivariateMetric:
+    def __init__(self, results, statistic, observed):
+        self.results = np.array(results)
+        self.statistic = statistic
+        self.observed = observed
+
+    def se(self):
+        return np.apply_along_axis(np.std, 0, self.results, ddof=0)
+    
+    def point_estimate(self, center):
+        return np.apply_along_axis(center, 0, self.results)
+
+    def plot(self, col, bins=30):
+        plt.hist(self.results[:,col], bins=bins)
+
+    def ci(self, col, confidence=0.95, kind='efron'):
+        quantile = 100*(1 - confidence)/2
+        res = self.results[:,col]
+        L, U = np.percentile(res, quantile), np.percentile(res, 100 - quantile)
+        if kind == 'efron':
+            return L, U
+        elif kind == 'loc_percentile':
+            return 2*self.observed - U, 2*self.observed - L
+        elif kind == 'scale_percentile':
+            return (self.observed ** 2 / U, self.observed ** 2 / L)
+        else:
+            raise Exception("unsupported ci type")
+            
