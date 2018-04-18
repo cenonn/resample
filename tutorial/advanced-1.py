54abf688b2b06fc18d0b96f0a1c77cc226f5bbb3
cenonn <rm.cenon@gmail.com>
2018-04-18 16:53:27 -0700

    rebuild

--- tutorial/advanced-1.py
+++ tutorial/advanced-1.py
@@ -3,5 +3,5 @@ import numpy as np
 import resample as rs
 
 data = pd.read_csv("score.csv")
-bootstrap = rs.boot(data[["mec", "vec"]], np.var)
-bootstrap.plot(col=1)
+bootstrap = rs.boot(data[["mec", "vec"]], np.cov)
+bootstrap.plot(col=0, row=0)
