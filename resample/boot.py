c7542e89d91e7bc5b840b7516218fc943a193410
cenonn <rm.cenon@gmail.com>
2018-04-18 17:14:58 -0700

    fixed multivariate situations and BCa

--- resample/boot.py
+++ resample/boot.py
@@ -10,20 +10,24 @@ def boot(data, statistic, group_cols=None, output_cols=None, r=10000, **kwargs):
 
         :param data: input data to be bootstrapped
         :param statistic: statistic to be calculated
+        :param group_cols: group columns for data, default is none
+        :param output_cols: output columns for data, default is none
         :param r: number of bootstrap replications
         :type data: np.array, pd.Series, np.DataFrame
         :type statistic: Statistic, function
-        :type t: int
+        :type group_cols: list
+        :type output_cols: list
+        :type r: int
         :return: results from the bootstrap simulation
-        :rtype: UnivariateResult, MultivariateResult
+        :rtype: Results
 
     """
     if isinstance(statistic, Statistic):
-        func = statistic.func
+        func = lambda *args: statistic.func(*args, **kwargs)
         if not statistic.is_valid:
             raise Warning("results from bootstrap may not be valid")
     else:
-        func = statistic
+        func = lambda *args: statistic(*args, **kwargs)
 
     r = int(r)  # make sure r is an int
     results = []
@@ -33,48 +37,62 @@ def boot(data, statistic, group_cols=None, output_cols=None, r=10000, **kwargs):
         if data.ndim == 1:
             for _ in range(r):
                 boot_sample = np.random.choice(data, sample_size, replace=True)
-                results.append(func(boot_sample, **kwargs))
-            return Results(results, func, func(data, **kwargs), data)
+                results.append(func(boot_sample))
+            return Results(results, func, func(data), data)
         else:
             raise Exception("numpy array must contain only 1 variable")
     elif isinstance(data, pd.Series):
         for _ in range(r):
             boot_sample = data.sample(n=sample_size, replace=True)
-            results.append(func(boot_sample, **kwargs))
-        return Results(results, func, func(data, **kwargs), data)
+            results.append(func(boot_sample))
+        return Results(results, func, func(data), data)
     elif isinstance(data, pd.DataFrame):
         if len(data.columns) == 1:
             for _ in range(r):
                 boot_sample = data.sample(n=sample_size, replace=True).as_matrix()
-                results.append(func(boot_sample, **kwargs))
-            return Results(results, func, func(data, **kwargs), data)
+                results.append(func(boot_sample))
+            return Results(results, func, func(data), data)
         else:
             if group_cols is None:
                 for _ in range(r):
                     # frac=1 to return sample of same size
                     boot_sample = data.sample(frac=1, replace=True)
                     if output_cols is None:
-                        results.append(func(boot_sample, **kwargs))
+                        results.append(func(boot_sample))
                     else:
-                        results.append(func(boot_sample[boot_sample.columns.difference(output_cols)], boot_sample[output_cols],**kwargs))
+                        X = boot_sample[list(boot_sample.columns.difference(output_cols))]
+                        if len(output_cols) == 1:
+                            y = boot_sample[output_cols[0]]
+                        else:
+                            y = boot_sample[output_cols]
+                        current_res = func(X, y)
+                        results.append(current_res)
                 if output_cols is None:
-                    return Results(results, func, func(data, **kwargs), data)
+                    return Results(results, func, func(data), data)
                 else:
-                    return Results(results, func, func(boot_sample[boot_sample.columns.difference(output_cols)], boot_sample[output_cols],**kwargs), data)
+                    X = data[data.columns.difference(output_cols)]
+                    if len(output_cols) == 1:
+                        y = boot_sample[output_cols[0]]
+                    else:
+                        y = boot_sample[output_cols]
+                    return Results(results, func, func(X, y), data, output_cols=output_cols)
             else:
-                # TODO
                 indices = data.reset_index().groupby(group_cols)["index"].apply(list).to_dict()
                 grouped_data = {}
                 for key, val in indices.items():
-                    grouped_data[key] = data.iloc[val][data.iloc[val].columns.difference(group_cols)]
-
+                    grouped_data[key] = data.loc[val][data.loc[val].columns.difference(group_cols)]
+                observed = np.asarray(func(*list(grouped_data.values())))
                 for _ in range(r):
-                    current_res = []
-                    for key, val in grouped_data.items():
+                    current_iter = []
+                    for _, val in grouped_data.items():
                         boot_sample = val.sample(frac=1, replace=True)
-                        current_res.append(boot_sample)
-                    results.append(func(*current_res, **kwargs).as_matrix())
-                return Results(results, func, func(*list(grouped_data.values()), **kwargs).as_matrix(), grouped_data)
+                        current_iter.append(boot_sample)
+                    if len(observed) == 1:
+                        current_res = func(*current_iter)[0]
+                    else:
+                        current_res = func(*current_iter)
+                    results.append(current_res)
+                return Results(results, func, observed, data, group_cols=group_cols)
     else:
         raise Exception("data type not supported")
 
--- resample/result.py
+++ resample/result.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from scipy.stats import norm
@@ -9,21 +10,29 @@ from scipy.stats import norm
 class Results:
     """Defines an object for bootstrap results
 
-            :param results: array that contains the estimate \
-                for every bootstrap replication
-            :param statistic: statistic to be calculated
-            :param observed: the sample statistic from the original data
-            :param data: the original data
-            :type results: np.array
-            :type statistic: function
-            :type observed: int, float
-            :type data: np.array, pd.Series, pd.DataFrame
+        :param results: array that contains the estimate \
+            for every bootstrap replication
+        :param statistic: statistic to be calculated
+        :param observed: the sample statistic from the original data
+        :param data: the original data
+        :param group_cols: group columns for data, default is none
+        :param output_cols: output columns for data, default is none
+        :type results: np.array
+        :type statistic: function
+        :type observed: int, float, np.array
+        :type data: np.array, pd.Series, pd.DataFrame
+        :type group_cols: list
+        :type output_cols: list
     """
-    def __init__(self, results, statistic, observed, data):
+    def __init__(self, results, statistic, observed, data, group_cols=None, output_cols=None):
         self.results = np.array(results)
         self.statistic = statistic
         self.observed = observed
-        self.data = np.array(data)
+        self.data = data
+        self.group_cols = group_cols
+        self.output_cols = output_cols
+        self.ndim = self.results.ndim
+        self.shape = self.results.shape
 
     def se(self):
         """Bootstrap estimate of standard error
@@ -41,7 +50,7 @@ class Results:
         """
         return np.mean(self.results) - self.observed
 
-    def point_estimate(self, center, correction=False):
+    def point_estimate(self, center, correction=False, **kwargs):
         """Point estimate from the bootstrap distribution
 
             :param center: measure of center to use
@@ -52,12 +61,14 @@ class Results:
             :rtype: float, int
 
         """
+        center_func = lambda x: center(x, **kwargs)
         if not correction:
-            return np.apply_along_axis(center, 0, self.results)
+            return np.apply_along_axis(center_func, 0, self.results)
         else:
-            return np.apply_along_axis(center, 0, self.results) - self.bias()
+            return np.apply_along_axis(center_func, 0, self.results) - self.bias()
 
-    def plot(self, col=None, row=None, bins=30):
+    # maybe change col behavior?
+    def plot(self, col=None, row=None, bins=30, **kwargs):
         """Create a histogram of the bootstrap distribution
 
             :param bins: number of bins for the histogram
@@ -66,26 +77,31 @@ class Results:
             :type col: int
         """
         if col is None and row is None:
-            plt.hist(self.results, bins=bins)
+            plt.hist(self.results, bins=bins, **kwargs)
         elif col is not None and row is None:
-            plt.hist(self.results[col], bins=bins)
+            plt.hist(self.results[:, col], bins=bins, **kwargs)
         elif col is not None and row is not None:
-            plt.hist(self.results[:, row, col], bins=bins)
+            plt.hist(self.results[:, row, col], bins=bins, **kwargs)
         else:
-            raise Exception("please provide at least column to plot")
+            raise Exception("provide at least column to plot")
 
     def ci(self, col=None, row=None, confidence=0.95, kind='efron'):
         """Calculate an interval estimate of the statistic
 
+            :param col: column to plot
+            :param row: row to plot
             :param confidence: the confidence level of the estimate, \
                 between 0.0 and 1.0
-            :param kind: type of interval to calculate, either efron or \
-                percentile intervals
+            :param kind: type of interval to calculate, either efron, \
+                BCa, location percentile, or scale percentile
+            :type col: int
+            :type row: int
             :type confidence: float
             :type kind: string
             :return: the confidence interval
             :rtype: tuple
         """
+        data = self.data
         if col is None and row is None:
             res = self.results
             obs = self.observed
@@ -107,12 +123,44 @@ class Results:
             return obs ** 2 / U, obs ** 2 / L
         elif kind == 'BCa':
             # calculate bias-correction
-            z_hat_nought = norm.ppf(sum(res < obs) / len(res))
+            count = sum(res < obs)
+            # may need to add adjustment to prevent norm.ppf(0) or norm.ppf(1) at small r
+            z_hat_nought = norm.ppf(count / len(res))
 
             # calculate acceleration
-            index_range = np.arange(0, len(self.data))
-            jack_index = (np.delete(index_range, i) for i in index_range)
-            theta_i = [np.mean(self.data[i]) for i in jack_index]
+            if isinstance(data, pd.DataFrame):
+                theta_i = []
+                for i, _ in data.iterrows():
+                    current_iter = data.drop(i)
+                    if self.group_cols is None:
+                        if self.output_cols is None:
+                            theta_i.append(self.statistic(current_iter))
+                        else:
+                            X = current_iter[current_iter.columns.difference(self.output_cols)]
+                            if len(self.output_cols) == 1:
+                                y = current_iter[self.output_cols[0]]
+                            else:
+                                y = current_iter[self.output_cols]
+
+                            if col is None and row is None:
+                                current_res = self.statistic(X, y)
+                            elif col is not None and row is None:
+                                current_res = self.statistic(X, y)[col]
+                            else:
+                                current_res = self.statistic(X, y)[row, col]
+                            theta_i.append(current_res)
+                    else:
+                        indices = current_iter.reset_index().groupby(self.group_cols)["index"].apply(list).to_dict()
+                        grouped_data = {}
+                        for key, val in indices.items():
+                            grouped_data[key] = current_iter.loc[val][current_iter.loc[val].columns.difference(self.group_cols)]
+                        current_res = self.statistic(*list(grouped_data.values()))
+                        theta_i.append(current_res)
+            else:
+                index_range = np.arange(0, len(data))
+                jack_index = (np.delete(index_range, i) for i in index_range)
+                theta_i = [self.statistic(data[i]) for i in jack_index]
+
             theta_dot = np.mean(theta_i)
             a_hat_num = np.sum((theta_dot - theta_i) ** 3)
             a_hat_den = 6.0 * np.sum((theta_dot - theta_i) ** 2) ** 1.5
